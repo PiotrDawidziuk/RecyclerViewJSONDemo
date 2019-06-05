14e49e144c22c8477ae682c50e9aa38f167857b9
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2019-06-05 19:56:09 +0200

    ExampleAdapter

---
+++ app/src/main/java/pl/piotrdawidziuk/recyclerviewjsondemo/ExampleAdapter.java
@@ -0,0 +1,79 @@
+package pl.piotrdawidziuk.recyclerviewjsondemo;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class ExampleAdapter extends RecyclerView.Adapter<ExampleAdapter.ExampleViewHolder> {
+
+
+    private Context mContext;
+    private ArrayList<ExampleItem> mExempleList;
+
+
+
+
+    public ExampleAdapter(Context mContext, ArrayList<ExampleItem> mExempleList) {
+        this.mContext = mContext;
+        this.mExempleList = mExempleList;
+    }
+
+    @NonNull
+    @Override
+    public ExampleViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(mContext).inflate(R.layout.example_item, viewGroup, false);
+        return new ExampleViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ExampleViewHolder exampleViewHolder, int i) {
+
+        ExampleItem currentItem = mExempleList.get(i);
+
+        String imageUrl = currentItem.getmImageUrl();
+        String creatorName = currentItem.getmCreator();
+        int likeCount = currentItem.getmLikes();
+
+        exampleViewHolder.mTextViewCreator.setText(creatorName);
+        exampleViewHolder.mTextViewLikes.setText(likeCount);
+
+        Picasso.get().load(imageUrl).fit().centerInside().into(exampleViewHolder.mImageView);
+
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mExempleList.size();
+    }
+
+    public class ExampleViewHolder extends RecyclerView.ViewHolder{
+
+        public ImageView mImageView;
+        public TextView mTextViewCreator;
+        public TextView mTextViewLikes;
+
+
+
+        public ExampleViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            mImageView = itemView.findViewById(R.id.image_view);
+            mTextViewCreator = itemView.findViewById(R.id.text_view_creator);
+            mTextViewLikes = itemView.findViewById(R.id.text_view_likes);
+
+
+        }
+    }
+}
